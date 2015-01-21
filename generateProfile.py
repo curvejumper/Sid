4c950756c9a2c36b4c7feb8b8d5b2923622cc045
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-01-20 16:39:57 -0800

    Added GenerateProfile class. This class will be used to either create a new profile for SID or recognize if there is already a profile to read from.

---
+++ generateProfile.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import json
+
+
+class GenerateProfile(object):
+    
+    def __init__(self, profile):
+        self.profile = profile
+    
+    
+    
+    def __init__(self):
+        createProfile()
+        
+        
+        
+        
+    def createProfile(self):
+        
+           
